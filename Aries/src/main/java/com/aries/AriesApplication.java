10a090ad144a35a962450b15e390226355cbff42
LAKSHYA BHARDWAJ <lakshyabhardwaj200315@gmail.com>
2026-04-17 20:13:12 +0530

    feat: initialize project structure and generate gRPC service classes for rate limiting

---
+++ Aries/src/main/java/com/aries/AriesApplication.java
@@ -0,0 +1,12 @@
+package com.aries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AriesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AriesApplication.class, args);
+    }
+}
---
+++ Aries/src/test/java/com/aries/AriesApplicationTests.java
@@ -0,0 +1,12 @@
+package com.aries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AriesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
